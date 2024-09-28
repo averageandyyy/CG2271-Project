9975fb617e61cdc9a62661b180299e040f3b383e
averageandyyy <59792108+averageandyyy@users.noreply.github.com>
2024-09-28 22:06:39 +0800

    Trial src and include with uKeilVision

---
+++ Objects/test.d
@@ -0,0 +1,3 @@
+.\objects\test.o: src\test\test.c
+.\objects\test.o: .\include\test/test.h
+.\objects\test.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include "test/test.h"
+
+int main(void)
+{
+    printHelloWorld();
+    while (1)
+    {
+
+    }
+}
