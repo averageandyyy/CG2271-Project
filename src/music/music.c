7c934707ede7afdc0c62d995a76edcf5f9b13b85
averageandyyy <59792108+averageandyyy@users.noreply.github.com>
2024-11-06 06:17:36 +0800

    Fix music - Change prescaler used from 128 to 8, using a higher PWM frequency - Updates notes MOD values to match the correct frequencies

--- src/music/music.c
+++ src/music/music.c
@@ -1,36 +1,38 @@
 #include "music/music.h"
 
 int mary[27] = {
-    E, D_NOTE, C, D_NOTE, E, E, E, D_NOTE, D_NOTE, D_NOTE, D_NOTE, E, G, G, E, D_NOTE, C, D_NOTE, E, E, E, C, D_NOTE, D_NOTE, E, D_NOTE, C
-};
+    E, D_NOTE, C, D_NOTE, E, E, E, D_NOTE, D_NOTE, D_NOTE, D_NOTE, E, G, G, E, D_NOTE, C, D_NOTE, E, E, E, C, D_NOTE, D_NOTE, E, D_NOTE, C};
 
 int birthday[25] = {
-    G4, G4, A4, G4, C5, B4,  
-    G4, G4, A4, G4, D5, C5,  
-    G4, G4, G5, E5, C5, B4, A4,  
-    F5, F5, E5, C5, D5, C5
-};
+    G4, G4, A4, G4, C5, B4,
+    G4, G4, A4, G4, D5, C5,
+    G4, G4, G5, E5, C5, B4, A4,
+    F5, F5, E5, C5, D5, C5};
 
-void initMusic(void) {
+void initMusic(void)
+{
     initMusicGPIO();
     initMusicTimer();
 }
 
-void initMusicGPIO(void) {
+void initMusicGPIO(void)
+{
     SIM_SCGC5 |= SIM_SCGC5_PORTE_MASK;
 
     PORTE_PCR(31) &= ~PORT_PCR_MUX_MASK;
     PORTE_PCR(31) |= PORT_PCR_MUX(3);
 }
 
-void initMusicTimer(void) {
+void initMusicTimer(void)
+{
     SIM_SCGC6 |= SIM_SCGC6_TPM0_MASK;
 
     SIM_SOPT2 &= ~SIM_SOPT2_TPMSRC_MASK;
     SIM_SOPT2 |= SIM_SOPT2_TPMSRC(1);
 
     TPM0_SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
-    TPM0_SC |= (TPM_SC_CMOD(1) | TPM_SC_PS(7));  // ps=128
+    // TPM0_SC |= (TPM_SC_CMOD(1) | TPM_SC_PS(7));  // ps=128
+    TPM0_SC |= (TPM_SC_CMOD(1) | TPM_SC_PS(3)); // ps=8
     TPM0_SC &= ~TPM_SC_CPWMS_MASK;
 
     TPM0_C4SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK | (TPM_CnSC_MSA_MASK)));
@@ -39,18 +41,25 @@ void initMusicTimer(void) {
 
 bool isMary = true;
 
-void music_thread(void* argument) {
-    for (;;) {
-        if (isMary) {
-            for (int i = 0; i < 27 && isMary; i++) {
+void music_thread(void *argument)
+{
+    for (;;)
+    {
+        if (isMary)
+        {
+            for (int i = 0; i < 27 && isMary; i++)
+            {
                 TPM0_MOD = mary[i];
                 TPM0_C4V = mary[i] / 2;
                 osDelay(300);
                 TPM0_C4V = 0;
                 osDelay(25);
             }
-        } else {
-            for (int i = 0; i < 25 && !isMary; i++) {
+        }
+        else
+        {
+            for (int i = 0; i < 25 && !isMary; i++)
+            {
                 TPM0_MOD = birthday[i];
                 TPM0_C4V = birthday[i] / 2;
                 osDelay(500);
@@ -61,6 +70,7 @@ void music_thread(void* argument) {
     }
 }
 
-void initMusicRTOS(void) {
+void initMusicRTOS(void)
+{
     osThreadNew(music_thread, NULL, NULL);
 }
--- src/music/music.h
+++ src/music/music.h
@@ -1,7 +1,7 @@
 /*
-* Motors are using TPM1 and TPM2, so music PWM must use TPM0
-* Use PTE31 corresponds to TPM0_CH4 under ALT3
-*/
+ * Motors are using TPM1 and TPM2, so music PWM must use TPM0
+ * Use PTE31 corresponds to TPM0_CH4 under ALT3
+ */
 
 #ifndef MUSIC_H
 #define MUSIC_H
@@ -14,30 +14,29 @@
 #include "lights/lights.h"
 
 #define MUSIC_PIN 31
-#define C 1431
-#define D_NOTE 1275
-#define E 1136
-#define F 1074
-#define G 956
-#define A 852
-#define B 759
+#define C 2294
+#define D_NOTE 2044
+#define E 1820
+#define F 1717
+#define G 1531
+#define A 1364
+#define B 1215
 
-#define C4 1431  
-#define D4 1275  
-#define E4 1136  
-#define F4 1074  
-#define G4 956   
-#define A4 852   
-#define B4 759   
-#define C5 717   
-#define D5 639   
-#define E5 569   
-#define F5 537   
-#define G5 478   
+#define C4 2294
+#define D4 2044
+#define E4 1820
+#define F4 1717
+#define G4 1531
+#define A4 1364
+#define B4 1215
+#define C5 1145
+#define D5 1022
+#define E5 912
+#define F5 859
+#define G5 765
 
 extern int mary[27];
 extern int birthday[25];
-
 
 void initMusic(void);
 void initMusicGPIO(void);
@@ -45,7 +44,7 @@ void initMusicTimer(void);
 
 extern bool isMary;
 
-void music_thread(void* argument);
+void music_thread(void *argument);
 
 void initMusicRTOS(void);
 
